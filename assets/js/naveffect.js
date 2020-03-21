a4f293d211e264b5209c3886ad2b084c19f7361c
Alain Reguera Delgado <areguera@centosproject.org>
2020-03-21 16:04:27 -0300

    Update navigation bar, pages, icons and scripts
    - Previously, when the page was reduced and viewed in small devices like
      cellphones the navigation bar menu was transparent over the page title
      making links and title both unreadable. This update opaques the navbar
      background color to make text visible on small devices (window width
      <= 767px). Thanks to Niels de Vos (@nixpanic) for reporting this issue
      on Twitter.
    
    - Previously, the background color of the menu item and its dropdown box
      were using different colors. This update changes the background color
      of the menu item to create a stronger visual relation with its
      dropdown box.
    
    - Previously, the navigation bar was using texts only for links. This
      update adds icons on the left side of texts to improve their visual
      recognition.
    
    - Previously, social media icons were using fontawasome4. This update
      changes social media icons to use fontawasome5.
    
    - Previously, when you scrolled the page down and reload the page
      (Ctrl+r), the navigation bar shows itself with a transparent
      background that makes its content unreadable. This update changes the
      js scripts to always opaque the navigation bar when it is in a
      position greater than 10px (the start point of navbar scrolled
      effect).
    
    - Previously, when the window was stretched, the right column in the
      page layout left a blank space on the right side of text.  This update
      fixes that issue setting the right column in the page layout to expand
      as needed when the window is stretched.
    
    - Remove unused files.

--- assets/js/naveffect.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(document).scrollTop() > 10) {
-            $("nav").addClass("scrolled");
-        } else {
-            $("nav").removeClass("scrolled");
-        }
-    });
-});
